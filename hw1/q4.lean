f0673621a1995a7feed72ae413ee3d9260021bca
cs511 <celerond@msn.com>
2023-09-13 22:58:43 -0400

    ..

--- hw1/q4.lean
+++ hw1/q4.lean
@@ -6,7 +6,7 @@ import Library.Tactic.Cancel
 
 
 -- 4a
-example {a b : ℤ} (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 :=
+example {a b : ℤ} (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 := by
   calc
     a = 2 * b + 5 := by rw [h1]
     _ = 2 * 3 + 5 := by rw [h2]
@@ -14,7 +14,7 @@ example {a b : ℤ} (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 :=
 
 
 -- 4b
-example {x : ℤ} (h1 : x + 4 = 2) : x = -2 :=
+example {x : ℤ} (h1 : x + 4 = 2) : x = -2 := by
   calc
     x = x + 4 - 4 := by ring
     _ = 2 - 4 := by rw [h1]
@@ -22,7 +22,7 @@ example {x : ℤ} (h1 : x + 4 = 2) : x = -2 :=
 
 
 -- 4c
-example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 :=
+example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 := by
   calc
     a = (a - 5 * b) + 5 * b := by ring
     _ = 4 + 5 * b := by rw [h1]
