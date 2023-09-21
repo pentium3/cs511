730fe3056d22029898b244c454f7a646c72b7164
cs511 <celerond@msn.com>
2023-09-21 00:04:27 -0400

    hw2 q6

--- hw2/q6.lean
+++ hw2/q6.lean
@@ -23,5 +23,12 @@ example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
     
 
 --6c
--- example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
---  sorry
+example {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
+  calc
+     (x ^ 3) - 8 * (x ^ 2) + 2 * x = x * (x ^ 2) - 8 * (x ^ 2) + 2 * x := by ring
+     _ ≥ 9 * (x ^ 2) - 8 * (x ^ 2) + 2 * x := by rel [hx]
+     _ = (x ^ 2) + 2 * x := by ring
+     _ ≥ (9 ^ 2) + 2 * 9 := by rel [hx]
+     _ ≥ 3 := by numbers
+
+
