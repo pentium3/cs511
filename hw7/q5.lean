dcbfc31a3846c508aeea9c96468a3d041c0421e4
cs511 <celerond@msn.com>
2023-10-25 23:55:18 -0400

    hw7

---
+++ hw7/q5.lean
@@ -0,0 +1,37 @@
+
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.IntervalCases
+import Library.Theory.Comparison
+import Library.Theory.Parity
+import Library.Theory.Prime
+import Library.Tactic.Rel
+import Library.Tactic.ModCases
+import Library.Tactic.Extra
+import Library.Tactic.Numbers
+import Library.Tactic.Addarith
+import Library.Tactic.Cancel
+import Library.Tactic.Use
+
+
+-- 5a
+example {p : ℕ} (k : ℕ) (hk1 : k ≠ 1) (hkp : k ≠ p) (hk : k ∣ p) : ¬ Prime p := by
+  dsimp [Prime]
+  push_neg
+  intros h
+  use k
+  constructor
+  · apply hk
+  constructor
+  · apply hk1
+  · apply hkp
+
+
+
+-- 5b
+example {p : ℕ} (hp : ¬ Prime p) (hp2 : 2 ≤ p) : ∃ m, 2 ≤ m ∧ m < p ∧ m ∣ p := by
+  have H : ¬ (∀ (m : ℕ), 2 ≤ m → m < p → ¬m ∣ p)
+  · intro H
+    have hpp := prime_test hp2 H
+    contradiction
+  push_neg at H
+  apply H
